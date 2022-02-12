99945b939b22e32bdae290e37e8940f1999905fc
vlreddy123 <99265985+vlreddy123@users.noreply.github.com>
2022-02-12 21:51:03 +0530

    Create eight.py

---
+++ eight.py
@@ -0,0 +1,10 @@
+x=float(input("enter the any 1value")) 
+y=float(input("2enter the any value")) 
+if x>y:
+ Print("1st value is big")
+else:
+ Print("2nh value is big") 
+if x==y:
+ Print("two values are equal ¡¡") 
+else:
+ Print("two numbers are not equal")
